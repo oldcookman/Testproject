d1f2f70902c55a38c911458abf5272f386f75274
admin <1449036299@qq.com>
2019-07-02 14:57:49 +0800

    测试上传

---
+++ BlogInfoSys/src/main/java/com/bdqn/demo/Test.java
@@ -0,0 +1,8 @@
+package com.bdqn.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("添加测试GitHub是否可用");
+        System.out.println("继续来啊");
+    }
+}
